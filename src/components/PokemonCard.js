61e9cd1488822a0fa8df5e8e8b707d1ae3d67b92
github username <github email address>
2019-06-10 22:02:13 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,42 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state={
+    front: true
+  }
+
+  handleFlip = () => {
+    this.setState({
+      front: !this.state.front
+    })
+  }
+
+
   render() {
+    // console.log(this.state);
+    const {sprites, name, stats} = this.props.poke
+    // console.log(this.props.poke);
     return (
-      <Card>
+      <Card onClick={this.handleFlip}>
         <div>
+          {
+            this.state.front ?
           <div className="image">
-            <img alt="oh no!" />
+            <img src={sprites.front} alt="oh no!" />
           </div>
+          :
+          <div className="image">
+            <img src={sprites.back} alt="oh no!" />
+          </div>
+        }
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {stats[5].value} hp
             </span>
           </div>
         </div>
