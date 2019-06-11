dcf420a89f844a373e581a556f67affc25713a42
github username <github email address>
2019-06-10 22:25:32 -0400

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -6,23 +6,90 @@ class PokemonForm extends React.Component {
     super()
 
     this.state = {
-      name: '',
-      hp: '',
-      frontUrl: '',
-      backUrl: ''
-    }
+        height: 10,
+        weight: 130,
+        id: 1,
+        name: '',
+        abilities: ["overgrow", "chlorophyll"],
+        moves: [],
+        stats: [],
+        types: ["grass", "poison"],
+        sprites: {
+          front: '',
+          back: ''    
+        }
+      }
+  }
+  handleNameChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.handleAddPokemon(this.state)
+    this.props.handleClick()
+  }
+
+  handleHpChange = (e) => {
+    this.setState({
+      stats: [
+        {
+          "value": 80,
+          "name": "special-defense"
+        },
+        {
+          "value": 80,
+          "name": "special-attack"
+        },
+        {
+          "value": 63,
+          "name": "defense"
+        },
+        {
+          "value": 62,
+          "name": "attack"
+        },
+        {
+          "value": 60,
+          "name": "speed"
+        },
+        {
+          "value": parseInt(e.target.value),
+          "name": "hp"
+        }
+      ]
+    })
+  }
+
+  handleSpriteFrontChange = (e) => {
+    this.setState({
+      sprites: {
+        ...this.state.sprites, front: e.target.value
+      }
+    })
+  }
+
+  handleSpriteBackChange = (e) => {
+    this.setState({
+      sprites: {
+        ...this.state.sprites, back: e.target.value
+      }
+    })
   }
 
   render() {
+    // console.log(this.state)
     return (
       <div>
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input onChange = {this.handleNameChange} fluid label="Name" placeholder="Name" name="name" />
+            <Form.Input onChange = {this.handleHpChange} fluid label="hp" placeholder="hp" name="value" />
+            <Form.Input onChange = {this.handleSpriteFrontChange} fluid label="Front Image URL" placeholder="url" name="front" />
+            <Form.Input onChange = {this.handleSpriteBackChange} fluid label="Back Image URL" placeholder="url" name="back" />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,20 +1,113 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import PokemonCollection from './PokemonCollection'
 import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
+import { Search, Button } from 'semantic-ui-react'
 import _ from 'lodash'
 
+const URL = `http://localhost:3000/pokemon`
+
 class PokemonPage extends React.Component {
+  state = {
+    pokemon: [],
+    searchTerm: '',
+    addPokemon: false,
+    loading: false
+  }
+
+  formatPokemon = (pokemon) => {
+    return this.filter(pokemon)
+  }
+
+  filter = (pokemon) => {
+    return pokemon.filter(pokemon => pokemon.name.toLowerCase().includes(this.state.searchTerm))
+  }
+
+  componentDidMount = () => {
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(pokemon => {
+      this.setState({
+        pokemon: pokemon,
+        loading: true
+      })
+    })
+  }
+
+  handleAddPokemon = (newPokemon) => {
+    // console.log(newPokemon)
+    // console.log(...this.state.pokemon)
+    // debugger
+    const {height, weight, id, name, abilities, moves, stats, types, sprites} = newPokemon
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        height: height,
+        weight: weight,
+        id: id,
+        name: name,
+        abilities: abilities,
+        moves: moves,
+        stats: stats,
+        types: types,
+        sprites: sprites
+      })
+    })
+    .then(res => res.json())
+    .then(newPoke => {
+      this.setState({
+      pokemon: [newPoke, ...this.state.pokemon]
+     })
+    })
+  
+    // this.setState({
+    //   pokemon: [newPokemon, ...this.state.pokemon]
+    // })
+}
+
+  handleChange = (e) => {
+    // console.log(e.target.value)
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+  handleClick = () => {
+    this.setState({
+      addPokemon: !this.state.addPokemon
+    })
+  }
   render() {
+    console.log(this.state.pokemon)
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.handleChange} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <Button onClick={this.handleClick} positive>Add Pokemon</Button>
+        <br/>
+        {this.state.loading ?
+
+        <Fragment>
+          {
+            this.state.addPokemon ?
+            <PokemonForm handleClick={this.handleClick} handleAddPokemon={this.handleAddPokemon} />
+            :
+            <PokemonCollection pokemon = {this.formatPokemon(this.state.pokemon)} />
+          }
+        </Fragment>
+        :
+        <Fragment>
+          <h1>Loading...</h1>
+        </Fragment>
+      
+        }
+        {/* <PokemonCollection pokemon = {this.formatPokemon(this.state.pokemon)} />
         <br />
-        <PokemonForm />
+        <PokemonForm /> */}
       </div>
     )
   }
