868a32179deebdd1721d6b865588b6e41be2d13f
Mallory Woods <malvwoods@gmail.com>
2019-06-11 09:25:19 -0400

    Done

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -6,7 +6,9 @@ class PokemonCollection extends React.Component {
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.props.allPokemon.map(poke =>
+          <PokemonCard pokemon={poke} name={poke.name} front={poke.sprites.front} back={poke.sprites.back} hp={poke.stats[5].value}/>
+        )}
       </Card.Group>
     )
   }
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,20 +1,103 @@
 import React from 'react'
 import PokemonCollection from './PokemonCollection'
 import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
+import { Search, Button } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemon: [],
+    searchTerm: ""
+  }
+
+  postPokemon = (poke) => {
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: poke.name.toLowerCase(),
+        sprites: {front: poke.frontUrl, back: poke.backUrl},
+        stats: [
+          {},
+          {},
+          {},
+          {},
+          {},
+          {value: 100}
+        ]
+      })
+    })
+    .then(res => res.json())
+    .then(this.updatePokemon)
+  }
+
+  updatePokemon = (pokemon) => {
+    this.setState({
+    pokemon: [...this.state.pokemon, pokemon]
+    })
+  }
+
+  handleSearchChange = (event) => {
+    this.state.pokemon.filter(pokemon => pokemon.name.includes(event.target.value))
+    this.setState({searchTerm: event.target.value})
+  }
+
+  filter = () => this.state.pokemon.filter(poke => poke.name.includes(this.state.searchTerm))
+
+
+  sort = (event) => {
+    let sortedPokemon;
+    switch(event.target.id){
+      case "name":
+       sortedPokemon = [...this.state.pokemon].sort((a, b) => (a.name > b.name) ? 1 : -1)
+      break;
+      case "id":
+       sortedPokemon = [...this.state.pokemon].sort((a, b) => (a.id > b.id) ? 1 : -1)
+       console.log(this.state.pokemon)
+      break;
+      case "hp":
+       sortedPokemon = [...this.state.pokemon].sort((a, b) => (a.stats[5].value > b.stats[5].value) ? 1 : -1)
+      break;
+    }
+    this.setState({pokemon: sortedPokemon})
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(fetchedPokemon => {
+      this.setState({
+        pokemon: fetchedPokemon
+      })
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.handleSearchChange} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <div className="button-container">
+          <Button onClick={this.sort} id="name" content="Sort By Name"/>
+          <br />
+          <Button onClick={this.sort} id="hp" content="Sort By HP"/>
+          <br />
+          <Button onClick={this.sort} id="id" content="Sort By ID"/>
+          <br />
+        </div>
         <br />
-        <PokemonForm />
+        <br />
+        <PokemonForm postPokemon={this.postPokemon} />
+        <br />
+        <br />
+        <PokemonCollection allPokemon={this.filter()} />
+        <br />
       </div>
     )
   }
