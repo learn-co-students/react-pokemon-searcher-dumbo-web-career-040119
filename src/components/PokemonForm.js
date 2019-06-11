580e57c8c652c654e6a0c6a3fca44afbc551044e
github username <github email address>
2019-06-10 20:32:25 -0400

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,16 +13,72 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:3000/pokemon', {
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        "height": 20,
+        "weight": 1000,
+        "name": this.state.name,
+        "abilities": ["overgrow", "chlorophyll"],
+        "moves": [],
+        "stats": [
+          {
+            "value": 100,
+            "name": "special-defense"
+          },
+          {
+            "value": 100,
+            "name": "special-attack"
+          },
+          {
+            "value": 83,
+            "name": "defense"
+          },
+          {
+            "value": 82,
+            "name": "attack"
+          },
+          {
+            "value": 80,
+            "name": "speed"
+          },
+          {
+            "value": this.state.hp,
+            "name": "hp"
+          }
+        ],
+        "types": ["grass", "poison"],
+        "sprites": {
+          "front": this.state.frontUrl,
+          "back": this.state.backUrl
+        }
+      })
+    }).then(response => response.json())
+    .then(pokemon => this.props.addNewPoke(pokemon))
+
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={this.handleChange}/>
+            <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={this.handleChange}/>
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" onChange={this.handleChange}/>
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" onChange={this.handleChange}/>
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,63 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemon: [],
+    searchInput: "",
+    sortName: false
+  }
+
+  componentDidMount = () => {
+    fetch('http://localhost:3000/pokemon')
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      pokemon: data
+    }))
+  }
+
+  handleSearch = (event) => {
+    this.setState({
+      searchInput: event.target.value
+    })
+  }
+
+  filter = () => {
+    return this.state.pokemon.filter(pokemon => {
+      return pokemon.name.toLowerCase().includes(this.state.searchInput.toLowerCase())
+    })
+  }
+
+  addNewPoke = (newPoke) => {
+    this.setState({
+      pokemon: [newPoke, ...this.state.pokemon]
+    })
+  }
+
+  handleClick = (event) => {
+    this.setState({
+      sortName: !this.state.sortName
+    })
+    this.state.sortName ? this.state.pokemon.sort((a,b) => {
+      return (a.name < b.name) ? 1 : -1
+    }) :
+    this.state.pokemon.sort((a,b)=> {
+      return (a.name > b.name) ? 1 : -1
+    })
+  }
+
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.handleSearch} showNoResults={false} />
+        <button onClick={this.handleClick}>Sort by Name</button>
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.filter()}/>
         <br />
-        <PokemonForm />
+        <PokemonForm addNewPoke={this.addNewPoke}/>
       </div>
     )
   }
